b64f5a316077c750e9b06cfbbe1d10951af79f6c
yogesh <yogesh.kamble102@gmail.com>
2014-11-16 20:37:26 +0530

    code for plotting graph

---
+++ plot_graph.py
@@ -0,0 +1,35 @@
+import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt
+
+CSV_FILE_PATH = "/home/yogesh/Coursera/DataScience/aamir_khan_analysis/aamir_movie_imdb.csv"
+
+def read_csv():
+    movie_file = pd.read_csv(CSV_FILE_PATH)
+    return movie_file
+
+def process_csv(pandas_csv_obj):
+    data_tuple_list = []
+    year_data = pandas_csv_obj.groupby("Year")
+    for name, group in year_data:
+        #data_dict[name] = [group["Name"].count()]
+        hit_count = 0
+        for rating in group.Rating:
+            if rating >= 7.0:
+                hit_count += 1
+        data_tuple = (name, group["Name"].count(), hit_count)
+        data_tuple_list.append(data_tuple)
+    return data_tuple_list
+
+def plot_graph(data_tuple_list):
+    df = pd.DataFrame(data_tuple_list, columns=['Year', 'Count', 'Hits'])
+    df.set_index('Year', inplace=True)
+    df.plot(kind='bar')
+    plt.show()
+    return
+
+def start():
+    movie_file = read_csv()
+    data_tuple_list = process_csv(movie_file)
+    #return data_tuple_list
+    plot_graph(data_tuple_list)
